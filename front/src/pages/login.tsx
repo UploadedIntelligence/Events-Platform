2b26216acd9426c07a4ca72f62351d032937e85a
kakvo <kakvoo1@gmail.com>
2025-10-08 13:57:12 +0100

    user can log in either via a google account or email and password

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,20 +1,67 @@
-import { Button } from '@mui/material';
+import { Button, TextField, Typography } from '@mui/material';
 import authClient from '../services/auth-client.ts';
 import { Navigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export function LoginPage() {
     const { data, error } = authClient.useSession();
 
-    async function signIn() {
-        const { data, error } = await authClient.signIn.social({
-            provider: 'google',
-            callbackURL: 'http://localhost:5173',
-            errorCallbackURL: 'http://localhost:5173',
+    const {
+        register,
+        watch,
+        formState: { errors, isValid },
+    } = useForm({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    });
+
+    if (error) {
+        console.log('authClient error:', error);
+    }
+
+    let user_email = watch('email');
+    let user_password = watch('password');
+
+    async function emailSingIn() {
+        const { data, error } = await authClient.signIn.email({
+            email: user_email,
+            password: user_password,
         });
-        console.log('Data', data);
+
+        console.log('manual sign in error:', data);
         if (error) {
-            console.log('Error:', error);
+            console.log('manual sign in error:', error);
         }
     }
-    return <>{data ? <Navigate to='/' /> : <Button onClick={signIn}>Sign in with Google</Button>}</>;
+
+    return (
+        <div>
+            {data ? (
+                <Navigate to="/" />
+            ) : (
+                <div className="login">
+                    <Typography>Log In</Typography>
+                    <form className="login" action={emailSingIn}>
+                        <TextField
+                            label="Email"
+                            error={!!errors.email?.message}
+                            helperText={errors.email?.message}
+                            {...register('email', { required: 'Field required' })}
+                        />
+                        <TextField
+                            label="Password"
+                            error={!!errors.password?.message}
+                            helperText={errors.password?.message}
+                            {...register('password', { required: 'Field required' })}
+                        />
+                        <Button type="submit" disabled={!isValid} variant="contained">
+                            Submit
+                        </Button>
+                    </form>
+                </div>
+            )}
+        </div>
+    );
 }
