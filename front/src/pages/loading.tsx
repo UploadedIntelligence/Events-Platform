015b0e925326b890dec04e42ae63d5f96eeb31b1
kakvo <kakvoo1@gmail.com>
2025-10-08 13:56:23 +0100

    added spinner during user details loading

---
+++ front/src/pages/loading.tsx
@@ -0,0 +1,22 @@
+import '../Spinner.css';
+
+export function Spinner() {
+    return (
+        <div className="loadingio-spinner-spinner-ph19swaelxb">
+            <div className="ldio-dw14n2pywes">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
