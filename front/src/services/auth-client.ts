babf2b2fd0eccc7679b29d38ff45d0db5f6c12fb
kakvo <kakvoo1@gmail.com>
2025-10-13 20:40:26 +0100

    added user role to auth client.

--- front/src/services/auth-client.ts
+++ front/src/services/auth-client.ts
@@ -1,5 +1,15 @@
 import { createAuthClient } from 'better-auth/react';
+import { inferAdditionalFields } from 'better-auth/client/plugins';
 
 export default createAuthClient({
     baseURL: 'http://localhost:7000',
+    plugins: [
+        inferAdditionalFields({
+            user: {
+                role: {
+                    type: 'string',
+                },
+            },
+        }),
+    ],
 });
