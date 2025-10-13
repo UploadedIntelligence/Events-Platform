c580a6bd7d17750bdc85b5f01dde4b6de6140005
kakvo <kakvoo1@gmail.com>
2025-10-13 20:34:09 +0100

    Better auth now asks for calendar access. Access type is offline so that server can have access to refresh token. Prompt consent added for testing purposes

--- back/lib/auth.ts
+++ back/lib/auth.ts
@@ -21,6 +21,11 @@ export const auth = betterAuth({
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            scope: [
+                'https://www.googleapis.com/auth/calendar.events',
+            ],
+            accessType: 'offline',
+            prompt: 'consent',
         },
     },
     trustedOrigins: ['http://localhost:5173'],
