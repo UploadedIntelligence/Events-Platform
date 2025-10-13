8b920b58d000b6371893a7a6988037db6c9980ef
kakvo <kakvoo1@gmail.com>
2025-10-13 20:37:14 +0100

    Nav bar now handles events shown and dialog related to each event.

--- front/src/components/nav-bar.tsx
+++ front/src/components/nav-bar.tsx
@@ -1,28 +1,56 @@
 import axios from '../config/client.ts';
 import '../styles/nav-bar.scss';
-import { Button, CardActions, CardContent, Typography, AppBar, Toolbar } from '@mui/material';
+import { Button, AppBar, Toolbar, Menu, MenuItem } from '@mui/material';
 import authClient from '../services/auth-client.ts';
 import { Navigate, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, type MouseEvent } from 'react';
+import { AttendOrCancelEventDialog } from './attend-event-dialog.tsx';
+import { ShowEvents } from './show-events.tsx';
 
 export function NavBar() {
     const navigate = useNavigate();
     const { data } = authClient.useSession();
     const [events, setEvents] = useState<Array<any>>([]);
     const [visibleEvents, setVisibleEvents] = useState<boolean>(false);
+    const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+    const [selectedEvent, setSelectedEvent] = useState<any>(null);
+    const [isUpcoming, setIsUpcoming] = useState<boolean>(false);
+    const [isAttending, setIsAttending] = useState<boolean>(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
 
+
+    const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+
+        //
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+
+        //
+    };
+    //////////////////
     // check if user is staff/admin to create events
 
     async function logOut() {
         authClient.signOut();
     }
 
-    async function fetchEvents(pastOrFuture: 'past' | 'future') {
+    async function fetchEvents(pastFutureOrAttending: 'past' | 'future' | 'attending') {
         let events;
-        if (pastOrFuture === 'future') {
-            events = await axios.get('/upcoming-events');
+        if (pastFutureOrAttending === 'future') {
+            events = await axios.get('/upcoming-events', { withCredentials: true });
+            setIsUpcoming(true);
+            setIsAttending(false);
+        } else if (pastFutureOrAttending === 'past') {
+            events = await axios.get('/past-events', { withCredentials: true });
+            setIsUpcoming(false);
+            setIsAttending(false);
         } else {
-            events = await axios.get('/past-events');
+            events = await axios.get('/attending', { withCredentials: true });
+            setIsUpcoming(false);
+            setIsAttending(true);
         }
         setVisibleEvents(true);
         setEvents(events.data);
@@ -53,42 +81,63 @@ export function NavBar() {
                             <Button
                                 onClick={() => {
                                     fetchEvents('past');
+                                    navigate('/');
                                 }}
                             >
                                 Past Events
                             </Button>
-                            <Button onClick={logOut}>log out</Button>
+                            <Button
+                                onClick={() => {
+                                    fetchEvents('attending');
+                                    navigate('/');
+                                }}
+                            >
+                                Attending
+                            </Button>
+                            <Button
+                                id="basic-button"
+                                aria-controls={open ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
+                            >
+                                Dashboard
+                            </Button>
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                slotProps={{
+                                    list: {
+                                        'aria-labelledby': 'basic-button',
+                                    },
+                                }}
+                            >
+                                <MenuItem onClick={() => navigate('/user-settings')}>Settings</MenuItem>
+                                <MenuItem onClick={logOut}>Logout</MenuItem>
+                            </Menu>
                         </div>
                     </Toolbar>
                 </AppBar>
             ) : (
                 <Navigate to="/user/login" />
             )}
-            <div hidden={!visibleEvents}>
-                {events.map((event) => {
-                    const start = event.start.slice(0, 16).replace('T', ' at ');
-                    const end = event.end.slice(0, 16).replace('T', ' at ');
-                    return (
-                        <div>
-                            <CardContent sx={{ border: '3px solid #00802778', marginBottom: '8px' }}>
-                                <Typography variant="h5" component="div">
-                                    {event.name}
-                                </Typography>
-                                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-                                    {event.location}
-                                </Typography>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>start: {start}</Typography>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>end: {end}</Typography>
-                                <Typography variant="body2">{event.description}</Typography>
-                                <CardActions>
-                                    <Typography>Free event</Typography>
-                                    <Button size="small" variant='contained'>Attend</Button>
-                                </CardActions>
-                            </CardContent>
-                        </div>
-                    );
-                })}
-            </div>
+            <ShowEvents
+                visibleEvents={visibleEvents}
+                events={events}
+                setDialogOpen={setDialogOpen}
+                setSelectedEvent={setSelectedEvent}
+                isUpcoming={isUpcoming}
+                isAttending={isAttending}
+            />
+            <AttendOrCancelEventDialog
+                setEvents={setEvents}
+                dialogOpen={dialogOpen}
+                setDialogOpen={setDialogOpen}
+                selectedEvent={selectedEvent}
+                isAttending={isAttending}
+            />
         </div>
     );
 }
