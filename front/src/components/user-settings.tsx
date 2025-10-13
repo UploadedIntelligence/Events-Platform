f210304122c82e0fdfadbd76ca1890c98c799609
kakvo <kakvoo1@gmail.com>
2025-10-13 20:38:08 +0100

    User settings instantiated

---
+++ front/src/components/user-settings.tsx
@@ -0,0 +1,14 @@
+import { Card, Button } from '@mui/material';
+
+export function UserSettings() {
+    return (
+        <Card>
+
+            User settings here
+
+            <Button>
+
+            </Button>
+        </Card>
+    );
+}
