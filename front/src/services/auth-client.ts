ee43a836397cd7a494387072771673d260c7d301
kakvo <kakvoo1@gmail.com>
2025-10-20 10:29:30 +0100

    ---

--- front/src/services/auth-client.ts
+++ front/src/services/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from 'better-auth/react';
 import { adminClient, inferAdditionalFields } from 'better-auth/client/plugins';
 
 export default createAuthClient({
-    baseURL: 'https://events-platform-2-f7qv.onrender.com/api/auth',
+    baseURL: 'https://events-platform-2-f7qv.onrender.com',
     plugins: [
         inferAdditionalFields({
             user: {
