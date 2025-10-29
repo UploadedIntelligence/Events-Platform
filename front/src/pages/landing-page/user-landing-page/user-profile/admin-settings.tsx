8a7b133328e2e92a4e591c20da0835e2b8f9531a
kakvo <kakvoo1@gmail.com>
2025-10-29 19:44:05 +0000

    Reworked Admin settings page

--- front/src/pages/landing-page/user-landing-page/user-profile/admin-settings.tsx
+++ front/src/pages/landing-page/user-landing-page/user-profile/admin-settings.tsx
@@ -1,103 +1,40 @@
 import authClient from '../../../../services/auth-client.ts';
-import {
-    Card,
-    Button,
-    Typography,
-    CardActions,
-    TableContainer,
-    Table,
-    TableHead,
-    TableCell,
-    TableRow,
-    TableBody,
-} from '@mui/material';
-import axios from '../../../../config/client.ts';
+import { Card, Button, CardActions } from '@mui/material';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { ApplicationTable } from '../../../../components/application-table.tsx';
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
 
-interface Application {
+export interface Application {
     userEmail: string;
     status: 'pending' | 'approved' | 'rejected';
 }
 
 export function AdminSettings() {
-    const [applications, setApplications] = useState<Array<Application> | null>(null);
-
-    async function applicationResponse(applicant_email: string, response: 'approved' | 'rejected'): Promise<void> {
-        await axios.put('/application-response', { applicant_email, response }, { withCredentials: true });
-    }
-
-    async function fetchApplications(): Promise<void> {
-        const applications_response: Array<Application> = (await axios.get('/applications', { withCredentials: true }))
-            .data;
-        console.log(applications_response);
-        setApplications(applications_response);
-    }
-
     const { data } = authClient.useSession();
+    const [applicationsVisible, setApplicationsVisible] = useState<boolean>(false);
+
+    if (data?.user?.role !== 'admin') {
+        return <div>Forbidden</div>;
+    }
+
     return (
         <div>
-            {data?.user?.role === 'admin' ? (
-                <Card>
-                    <NavLink to="/user-profile">
-                        <Button>Go Back</Button>
-                    </NavLink>
+            <Card>
+                <NavLink to="/user-profile">
+                    <Button>Go Back</Button>
+                </NavLink>
 
-                    <CardActions style={{ display: 'grid' }}>
-                        <Button
-                            onClick={() => {
-                                fetchApplications();
-                            }}
-                        >
-                            View Applications
+                <CardActions style={{ display: 'grid' }}>
+                    <NavLink to={applicationsVisible ? '/admin-settings' : '/admin-settings/view-applications'}>
+                        <Button onClick={() => setApplicationsVisible(!applicationsVisible)}>
+                            {applicationsVisible ? 'Hide ' : 'View '}Applications
                         </Button>
-                        <TableContainer>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>User: </TableCell>
-                                        <TableCell>Status: </TableCell>
-                                        <TableCell align="center">Approve/Reject</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {applications &&
-                                        applications.map((application: Application, idx: number) => {
-                                            return (
-                                                <TableRow key={idx}>
-                                                    <TableCell>
-                                                        <Typography>{application.userEmail}</Typography>
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Typography>{application.status}</Typography>
-                                                    </TableCell>
-                                                    <TableCell sx={{ justifySelf: 'flex-end' }} align="center">
-                                                        <Button
-                                                            onClick={() =>
-                                                                applicationResponse(application.userEmail, 'approved')
-                                                            }
-                                                        >
-                                                            Approve
-                                                        </Button>
-                                                        <Button
-                                                            sx={{ marginLeft: '8px' }}
-                                                            color="secondary"
-                                                            onClick={() =>
-                                                                applicationResponse(application.userEmail, 'rejected')
-                                                            }
-                                                        >
-                                                            Reject
-                                                        </Button>
-                                                    </TableCell>
-                                                </TableRow>
-                                            );
-                                        })}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </CardActions>
-                </Card>
-            ) : null}
+                    </NavLink>
+                </CardActions>
+            </Card>
+            <Routes>
+                <Route path="/view-applications" element={<ApplicationTable />} />
+            </Routes>
         </div>
     );
 }
