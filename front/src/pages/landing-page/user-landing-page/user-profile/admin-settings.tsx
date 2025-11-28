6c2651b853b9732928c353d87e57c621ffecefb2
kakvo <kakvoo1@gmail.com>
2025-11-28 22:47:22 +0000

    extending role to all roles

--- front/src/pages/landing-page/user-landing-page/user-profile/admin-settings.tsx
+++ front/src/pages/landing-page/user-landing-page/user-profile/admin-settings.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 export interface Application {
     userEmail: string;
     status: 'pending' | 'approved' | 'rejected';
-    role: 'staff' | 'admin';
+    role: 'staff' | 'admin' | 'user';
 }
 
 export function AdminSettings() {
