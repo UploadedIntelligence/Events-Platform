5d5c648f9b9a56a8c51dcc25d931c57c6d907424
kakvo <kakvoo1@gmail.com>
2025-11-29 00:02:10 +0000

    prettier

--- front/src/pages/landing-page/user-landing-page/user-profile/index.tsx
+++ front/src/pages/landing-page/user-landing-page/user-profile/index.tsx
@@ -1,8 +1,8 @@
-import { Card, Button, CardContent, Typography} from '@mui/material';
+import { Card, Button, CardContent, Typography } from '@mui/material';
 import authClient from '../../../../services/auth-client.ts';
 import { Navigate, NavLink } from 'react-router-dom';
 import { RoleApplication } from '../../../../components/role-application.tsx';
-import { DeleteAccount } from "../../../../components/delete-account-dialog.tsx";
+import { DeleteAccount } from '../../../../components/delete-account-dialog.tsx';
 
 export function UserProfile() {
     const { data } = authClient.useSession();
@@ -11,9 +11,7 @@ export function UserProfile() {
             {data ? (
                 <Card>
                     <CardContent>
-                        <Typography>
-                            Hello, {data.user.name}
-                        </Typography>
+                        <Typography>Hello, {data.user.name}</Typography>
                         <div style={{ display: 'grid' }}>
                             {data.user.role === 'admin' && (
                                 <NavLink to="/admin-settings">
@@ -23,9 +21,7 @@ export function UserProfile() {
                             <NavLink to="/user-settings">
                                 <Button>User settings</Button>
                             </NavLink>
-                            {data.user.role === 'user' && (
-                                <RoleApplication/>
-                            )}
+                            {data.user.role === 'user' && <RoleApplication />}
                             <NavLink to="/user-history">
                                 <Button>History</Button>
                             </NavLink>
