87639afdc814356eda6630921905048e87aeea11
kakvo <kakvoo1@gmail.com>
2025-12-15 16:32:26 +0000

    prettier

--- back/utilities/user-session.ts
+++ back/utilities/user-session.ts
@@ -1,7 +1,7 @@
 import { auth } from '../lib/auth';
 import { fromNodeHeaders } from 'better-auth/node';
 import { Request } from 'express';
-import { IUserSession } from "./types";
+import { IUserSession } from './types';
 
 export async function currentSession(req: Request): Promise<IUserSession | null> {
     return await auth.api.getSession({
