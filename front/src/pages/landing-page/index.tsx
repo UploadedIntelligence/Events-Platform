57615f2f70dfe73f2f771d5075208ae77fcab55c
kakvo <kakvoo1@gmail.com>
2025-12-22 23:00:05 +0000

    App is now a separate component which contains component styling changes. Styling adjustments for different user browser theme settings + prettier

--- front/src/pages/landing-page/index.tsx
+++ front/src/pages/landing-page/index.tsx
@@ -1,63 +1,19 @@
 import '../../App.css';
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from './login.tsx';
-import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
-import { red, grey } from '@mui/material/colors';
-import authClient from '../../services/auth-client.ts';
 import { RegisterPage } from './register.tsx';
-import { Spinner } from '../../components/loading.tsx';
 import { UserLandingPage } from './user-landing-page';
+import { Paper } from '@mui/material';
 
-function LandingPage() {
-    const { isPending } = authClient.useSession();
-    const userTheme: boolean = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-    const theme = createTheme({
-        palette: {
-            mode: userTheme ? 'dark' : 'light',
-            primary: {
-                main: grey[600],
-            },
-            secondary: {
-                main: red[500],
-            },
-        },
-        components: {
-            MuiButton: {
-                defaultProps: {
-                    variant: 'outlined',
-                },
-                styleOverrides: {
-                    root: {
-                        fontWeight: '600',
-                    },
-                },
-            },
-            MuiAppBar: {
-                styleOverrides: {
-                    root: {
-                        background: 'inherit',
-                    },
-                },
-            },
-        },
-    });
-
-    if (isPending) {
-        return <Spinner />;
-    }
-
+export function LandingPage() {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
+        <Paper>
             <h1>Events Platform</h1>
             <Routes>
                 <Route path="/*" element={<UserLandingPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
             </Routes>
-        </ThemeProvider>
+        </Paper>
     );
 }
-
-export default LandingPage;
