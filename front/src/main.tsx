f8110208729a0522fd9c8fdab4aa72cd395331da
kakvo <kakvoo1@gmail.com>
2025-12-22 22:59:55 +0000

    App is now a separate component which contains component styling changes. Styling adjustments for different user browser theme settings + prettier

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import LandingPage from './pages/landing-page';
 import queryClient from '../src/services/tanstack-query-client.ts';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { App } from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
-                <LandingPage />
+                <App />
             </BrowserRouter>
         </QueryClientProvider>
     </StrictMode>,
