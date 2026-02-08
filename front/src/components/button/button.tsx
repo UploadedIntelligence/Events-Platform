3134b2da3dd486d46cdd8a897ec9ae47e5be2829
kakvo <kakvoo1@gmail.com>
2026-02-08 16:22:16 +0000

    updating button props

--- front/src/components/button/button.tsx
+++ front/src/components/button/button.tsx
@@ -2,13 +2,13 @@ import './button.scss';
 import React from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    children: string;
-    variant: 'primary' | 'secondary' | 'ghost';
+    children?: React.ReactNode;
+    variant?: 'primary' | 'secondary' | 'ghost';
 }
 
-export function EpButton({ children, ...remainingProps }: ButtonProps) {
+export function EpButton({ children, variant = 'primary', ...remainingProps }: ButtonProps) {
     return (
-        <button className={`EpButton EpButton--${remainingProps.variant}`} {...remainingProps}>
+        <button className={`EpButton EpButton--${variant}`} {...remainingProps}>
             {children}
         </button>
     );
