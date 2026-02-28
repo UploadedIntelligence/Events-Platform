ea384a46a489d142f86e5b9feb05e6e8354bb858
kakvo <kakvoo1@gmail.com>
2026-02-28 13:43:58 +0000

    added a new route and different styling to the background

--- front/src/pages/landing-page/user-landing-page/index.tsx
+++ front/src/pages/landing-page/user-landing-page/index.tsx
@@ -1,30 +1,25 @@
-import '../../../styles/nav-bar.scss';
-import '../../../App.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { UserEvents } from './user-events.tsx';
 import { CreateEvent } from './create-event.tsx';
 import { UserProfile } from './user-profile';
-import { NavBar } from '../../../components/nav-bar.tsx';
 import { AdminSettings } from './user-profile/admin-settings';
 import { UserSettings } from './user-profile/user-settings.tsx';
-import { getSession } from '../../../utilities/user-permissions.ts';
-import { BackgroundPaper } from "../../../components/background-parent-components/background-parent-components.tsx";
+import { EpPageContentContainer } from '../../../components/page-content-container/page-content-container.tsx';
 
 export function UserLandingPage() {
-    const user = getSession();
     return (
-        <BackgroundPaper>
-            {user ? <NavBar /> : <Navigate to="/login" />}
+        <EpPageContentContainer>
             <Routes>
                 <Route path="/create-event" element={<CreateEvent />} />
-                <Route path="/upcoming-events" element={<UserEvents eventUrl={'/upcoming-events'} />} />
-                <Route path="/past-events" element={<UserEvents eventUrl={'/past-events'} />} />
+                <Route path="/discover" element={<Navigate to="/discover/upcoming-events" />} />
+                <Route path="/discover/upcoming-events" element={<UserEvents eventUrl={'/upcoming-events'} />} />
+                <Route path="/discover/past-events" element={<UserEvents eventUrl={'/past-events'} />} />
                 <Route path="/attending" element={<UserEvents eventUrl={'/attending'} />} />
                 <Route path="/user-history" element={<UserEvents eventUrl={'/user-history'} />} />
                 <Route path="/user-profile" element={<UserProfile />} />
                 <Route path="/admin-settings/*" element={<AdminSettings />} />
                 <Route path="/user-settings" element={<UserSettings />} />
             </Routes>
-        </BackgroundPaper>
+        </EpPageContentContainer>
     );
 }
