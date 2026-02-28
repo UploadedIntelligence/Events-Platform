8a6c7b83b497ad850895262ceecf1187aced71b8
kakvo <kakvoo1@gmail.com>
2026-02-28 13:52:56 +0000

    created a custom navlink

--- front/src/components/nav-link/nav-link.tsx
+++ front/src/components/nav-link/nav-link.tsx
@@ -1,9 +1,13 @@
 import './nav-link.scss';
 import { NavLink, type NavLinkProps } from 'react-router-dom';
 
-export function EpNavLink({ children, ...remainingProps }: NavLinkProps) {
+interface CustomNavLinkProps extends NavLinkProps {
+    variant?: 'primary' | 'secondary';
+}
+
+export function EpNavLink({ children, variant = 'primary', ...remainingProps }: CustomNavLinkProps) {
     return (
-        <NavLink className="EpLinkComponent" {...remainingProps}>
+        <NavLink className={`EpLinkComponent EpLinkComponent--${variant}`} {...remainingProps}>
             {children}
         </NavLink>
     );
