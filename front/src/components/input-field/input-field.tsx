b43e0bae737529326eb0a1958046d6b83f26c8dc
kakvo <kakvoo1@gmail.com>
2026-02-28 13:52:38 +0000

    created a custom input field

--- front/src/components/input-field/input-field.tsx
+++ front/src/components/input-field/input-field.tsx
@@ -1,10 +1,42 @@
 import './input-field.scss';
 import React from 'react';
 
-export function EpInputField({ children, ...remainingProps }: React.ComponentPropsWithRef<'input'>) {
+export interface ICustomInputField extends React.ComponentPropsWithRef<'input'> {
+    error?: boolean;
+    helperText?: string;
+    startAdornment?: React.ReactNode;
+    endAdornment?: React.ReactNode;
+}
+
+export interface ICustomTextArea extends React.ComponentPropsWithRef<'textarea'> {
+    error?: boolean;
+    helperText?: string;
+    startAdornment?: React.ReactNode;
+    endAdornment?: React.ReactNode;
+    multiline?: boolean;
+    rows?: number;
+}
+
+export function EpInputField(props: ICustomInputField | ICustomTextArea) {
+    let error, helperText, startAdornment, endAdornment, multiline, rows, remainingProps;
+    if ('multiline' in props) {
+        ({ error, helperText, startAdornment, endAdornment, multiline, rows, ...remainingProps } = props);
+    } else {
+        ({ error, helperText, startAdornment, endAdornment, ...remainingProps } = props);
+    }
+
     return (
-        <input className="EpInputField" {...remainingProps}>
-            {children}
-        </input>
+        <div className="EpInputField">
+            <div className={`EpInputField-container ${error ? 'hasError' : ''}`}>
+                {startAdornment}
+                {multiline ? (
+                    <textarea className="EpInputField-input" rows={rows} {...remainingProps} />
+                ) : (
+                    <input className="EpInputField-input" {...remainingProps} />
+                )}
+                {endAdornment}
+            </div>
+            {error && <p className="EpInputField-error">{helperText}</p>}
+        </div>
     );
 }
