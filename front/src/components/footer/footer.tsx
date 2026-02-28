8a025d947d1c7268175202c92031e99ffdaa683d
kakvo <kakvoo1@gmail.com>
2026-02-28 13:51:51 +0000

    created a custom footer

---
+++ front/src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import './footer.scss';
+
+export function EpFooter() {
+    return (
+        <div className="EpFooter">
+            <p className="EpFooter-copyright">© 2024 EventHub. All rights reserved.</p>
+        </div>
+    );
+}
