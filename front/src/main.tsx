6c3b141d187019eea9039b59da83e2a360f42f7d
kakvo <kakvoo1@gmail.com>
2026-03-23 22:40:09 +0000

    added custom style reset for the site now that cssbaseline is removed

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,4 +1,5 @@
-import './index.css';
+import './style-reset.scss';
+import './index.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
