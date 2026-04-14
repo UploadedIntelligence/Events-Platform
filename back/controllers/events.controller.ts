09a64a6876b9605656a32a0d87b6b9e649bd0b8e
kakvo <kakvoo1@gmail.com>
2026-04-14 20:36:26 +0100

    changed the way events are fetched from the db

--- back/controllers/events.controller.ts
+++ back/controllers/events.controller.ts
@@ -1,30 +1,31 @@
 import { Request, Response } from 'express';
 import {
-    createEventService,
-    createGoogleEventService,
-    deleteGoogleCalendarEventService,
-    deleteGoogleEventService,
-    fetchAttendingEventsService,
-    fetchPastEventsService,
-    fetchSpecificEventService,
-    fetchUpcomingEventsService,
-    fetchUserHistoryService,
-    getUserGoogleEventService,
+    createPrismaEvent,
+    createGoogleEvent,
+    deleteGoogleCalendarEvent,
+    deleteGoogleEvent,
+    // fetchAttendingEvents,
+    fetchEvent,
+    // fetchUserHistory,
+    getUserGoogleEvent,
     hostEventImage,
-    insertGoogleCalendarEventService,
-    updateEventAttendanceService,
-    updateEventService,
+    insertGoogleCalendarEvent,
+    updateEventAttendance,
+    updateEvent,
+    fetchAllEvents,
 } from '../services/events.service';
 import { google } from 'googleapis';
 import { currentSession } from '../utilities/user-session';
 import { getUserAccountService, getUserGoogleClientService } from '../services/users.service';
 import { ZodError } from 'zod';
-import { EventInfoDO, IUserThirdPartyAccount, IUserSession } from '../utilities/types';
+import { type CreateEventDTO, type User, IUserThirdPartyAccount, IUserSession } from '../utilities/types';
 import * as z from 'zod';
 import { bufferFileType } from '../utilities/buffer-file-type';
+import dayjs from 'dayjs';
 
 const zEventInfo = z.object({
     name: z.string(),
+    organiser: z.string(),
     description: z.string(),
     location: z.string(),
     start: z.string().transform((value) => new Date(value)),
@@ -38,16 +39,15 @@ const zIsValidImageType = z.enum(['image/png', 'image/jpg']);
 const zAuthorizedEventCreatorRoles = z.enum(['admin', 'staff']);
 
 export async function getEvent(req: Request, res: Response) {
-    const session: IUserSession | null = await currentSession(req);
     const { event_id } = req.params;
 
-    if (!session || !event_id || !zAuthorizedEventCreatorRoles.safeParse(session.user.role).success) {
-        return res.status(401).json('Not authenticated');
+    if (!event_id) {
+        return res.status(401).json('Invalid event id');
     }
 
     try {
-        const event_details = await fetchSpecificEventService(event_id);
-        return res.status(200).json(event_details);
+        const eventDetails = await fetchEvent(event_id);
+        return res.status(200).json(eventDetails);
     } catch (e) {
         if (e instanceof ZodError) {
             return res.status(400).json(e.issues);
@@ -56,6 +56,72 @@ export async function getEvent(req: Request, res: Response) {
     }
 }
 
+export async function fetchEvents(req: Request, res: Response) {
+    const { fromDate, toDate } = req.query;
+    const { userId } = req.params;
+    const session: IUserSession | null = await currentSession(req);
+    let formattedFrom, formattedTo;
+
+    if (fromDate !== 'false' && typeof fromDate === 'string') {
+        formattedFrom = dayjs(fromDate);
+        if (!formattedFrom.isValid()) {
+            return res.status(400).json({ message: 'Invalid date format.' });
+        }
+    }
+
+    if (toDate !== 'false' && typeof toDate === 'string') {
+        formattedTo = dayjs(toDate);
+        if (!formattedTo.isValid()) {
+            return res.status(400).json({ message: 'Invalid date format.' });
+        }
+    }
+
+    try {
+        if (session?.user.id === userId) {
+            const events = await fetchAllEvents({
+                from: formattedFrom?.toDate(),
+                to: formattedTo?.toDate(),
+                userId: userId,
+            });
+            if (events) {
+                return res.status(200).send(events);
+            }
+        } else {
+            const events = await fetchAllEvents({ from: formattedFrom?.toDate(), to: formattedTo?.toDate() });
+            if (events) {
+                return res.status(200).send(events);
+            }
+        }
+        return res.status(400).json({ message: 'Something went wrong, please try again later.' });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// export async function fetchUserEvents(req: Request, res: Response) {
+//     const today = new Date();
+//     const session: IUserSession | null = await currentSession(req);
+//     const { eventType } = req.params;
+//
+//     console.log(eventType);
+//
+//     if (!session) {
+//         return res.status(401).json('Not authenticated');
+//     }
+//
+//     let events;
+//     try {
+//         if (eventType === 'attending') {
+//             events = await fetchAttendingEvents(today, session.user.id);
+//         } else if (eventType === 'history') {
+//             events = await fetchUserHistory(today, session.user.id);
+//         }
+//         return res.status(200).send(events);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+
 export async function createEvent(req: Request, res: Response) {
     const session: IUserSession | null = await currentSession(req);
 
@@ -63,11 +129,11 @@ export async function createEvent(req: Request, res: Response) {
         return res.status(401).json('Not authenticated');
     }
 
-    const eventInfo: EventInfoDO = zEventInfo.parse(req.body);
+    const eventInfo: CreateEventDTO = zEventInfo.parse(req.body);
 
     try {
         zIsDateValid.parse(eventInfo.start);
-        const createdEvent = await createEventService(eventInfo);
+        const createdEvent = await createPrismaEvent(eventInfo, session.user.id);
         return res.status(200).json({ message: 'Event successfully created', event: createdEvent });
     } catch (e) {
         if (e instanceof ZodError) {
@@ -106,35 +172,8 @@ export async function updateEventDetails(req: Request, res: Response) {
         return res.status(401).json('Not authenticated');
     }
 
-    await updateEventService(req.body, req.params.event_id!);
+    await updateEvent(req.body, req.params.event_id!);
     return res.status(200).json('Response');
-}
-
-export async function fetchEvents(req: Request, res: Response) {
-    const today = new Date();
-    const session: IUserSession | null = await currentSession(req);
-
-    if (!session) {
-        return res.status(401).json('Not authenticated');
-    }
-
-    try {
-        let events;
-        if (req.path === '/past-events') {
-            events = await fetchPastEventsService(today);
-        } else if (req.path === '/upcoming-events') {
-            events = await fetchUpcomingEventsService(today, session);
-        } else if (req.path === '/attending') {
-            events = await fetchAttendingEventsService(today, session);
-        } else if (req.path === '/user-history') {
-            events = await fetchUserHistoryService(today, session);
-        } else {
-            return res.status(400).json({ message: 'Something went wrong, please try again later.' });
-        }
-        return res.status(200).send(events);
-    } catch (e) {
-        console.log(e);
-    }
 }
 
 export async function attendOrCancelEvent(req: Request, res: Response) {
@@ -154,18 +193,18 @@ export async function attendOrCancelEvent(req: Request, res: Response) {
             calendar = google.calendar({ version: 'v3', auth: client });
         }
 
-        const updated_event = await updateEventAttendanceService(event_id, is_attending, session);
-        const google_event = await getUserGoogleEventService(session, event_id);
+        const updated_event = await updateEventAttendance(event_id, is_attending, session);
+        const google_event = await getUserGoogleEvent(session, event_id);
 
         if (is_attending && googleAccount && google_event && calendar) {
-            await deleteGoogleCalendarEventService(calendar, google_event);
-            await deleteGoogleEventService(google_event, session, event_id);
+            await deleteGoogleCalendarEvent(calendar, google_event);
+            await deleteGoogleEvent(google_event, session, event_id);
         } else if (googleAccount && calendar) {
-            const google_calendar_event = await insertGoogleCalendarEventService(calendar, updated_event);
+            const google_calendar_event = await insertGoogleCalendarEvent(calendar, updated_event);
             if (!google_calendar_event) {
                 return res.status(401).json('Not authenticated');
             }
-            await createGoogleEventService(google_calendar_event, session, event_id);
+            await createGoogleEvent(google_calendar_event, session, event_id);
         }
 
         return res.status(200).json({ message: 'Your attendance successfully updated' });
