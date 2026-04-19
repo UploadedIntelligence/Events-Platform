5c4e641b195f496f5ea00c1907ea9280e8b1b15c
kakvo <kakvoo1@gmail.com>
2026-04-19 11:42:44 +0100

    under development

--- front/src/pages/landing-page/user-profile/admin-settings/view-applications.tsx
+++ front/src/pages/landing-page/user-profile/admin-settings/view-applications.tsx
@@ -1,12 +1,13 @@
-import type { Application } from './index.tsx';
-import axios from '../../../../../config/client.ts';
-import { ApplicationTable } from '../../../../../components/application-table.tsx';
-import { Spinner } from '../../../../../components/spinner/spinner.tsx';
+import type { Application } from './admin-settings.route.tsx';
+import axios from '../../../../config/client.ts';
+import { ApplicationTable } from '../../../../components/application-table.tsx';
+import { Spinner } from '../../../../components/spinner/spinner.tsx';
 import { useQuery } from '@tanstack/react-query';
-import { getSession, type IUser } from '../../../../../utilities/user-permissions.ts';
+import { type IUser } from '../../../../utilities/user-permissions.ts';
+import { useRouteLoaderData } from 'react-router-dom';
 
 export function ViewApplications() {
-    const user: IUser | undefined = getSession();
+    const user: IUser | undefined = useRouteLoaderData('LandingPage');
 
     if (user?.role !== 'admin') {
         return <div>Forbidden</div>;
